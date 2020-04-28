1b82c317d66933838365dd6b5e1201f1568df291
Jaspinder Sabharwal <jas@jaspinder.net>
2020-04-28 07:07:43 +0000

    completed exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+average_annual_revenue = Store.average(:annual_revenue)
+
+annual_revenue = Store.arel_table[:annual_revenue]
+annual_revenue_1mil = Store.where(annual_revenue.gt(1000000)).size
+
+puts total_revenue
+puts average_annual_revenue
+puts annual_revenue_1mil
