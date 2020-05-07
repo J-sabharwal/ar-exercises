647f062735561984c698f755b61765f9d70a898d
Jaspinder Sabharwal <jas@jaspinder.net>
2020-05-07 05:45:15 +0000

    exercise 7 complete

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,15 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Alex", last_name: "Ryman", hourly_rate: 50)
+@store2.employees.create(first_name: "Kevin", last_name: "Smith", hourly_rate: 60)
+@store1.employees.create(first_name: "Tina", last_name: "Jefferson", hourly_rate: 60)
+@store1.employees.create(first_name: "Andrew", last_name: "Hardman", hourly_rate: 50)
+@store2.employees.create(first_name: "Sarah", last_name: "Pointer", hourly_rate: 40)
+@store1.employees.create(first_name: "Alan", last_name: "Dodd", hourly_rate: 50)
+@store2.employees.create(first_name: "Spencer", last_name: "Allen", hourly_rate: 60)
+@store2.employees.create(first_name: "Jennifer", last_name: "Jones", hourly_rate: 50)
+@store1.employees.create(first_name: "Jane", last_name: "Cook", hourly_rate: 60)
+@store2.employees.create(first_name: "Andrea", last_name: "Pence", hourly_rate: 50)
+
+puts Employee.count
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,9 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "What store name would you like to add?"
+store_name = gets.chomp
+
+addStore = Store.create(name: store_name)
+
+puts addStore.errors.full_messages
